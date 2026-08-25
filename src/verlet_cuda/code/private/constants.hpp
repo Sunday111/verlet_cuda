79b0e305f55829f238fbbd17645f936d3591bd3a
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-08-26 00:52:48 +0100

    Align CUDA toolchain and runtime

--- src/verlet_cuda/code/private/constants.hpp
+++ src/verlet_cuda/code/private/constants.hpp
@@ -16,14 +16,14 @@ inline constexpr size_t kGridMaxObjectsInCell = 4;
 // The size of each grid cell in world coordinates
 inline constexpr Vec2<size_t> kGridCellSize{1, 1};
 inline constexpr float kObjectRadius = 0.5f;
-static constexpr Vec2f init_corner{960, 520};
-inline constexpr edt::FloatRange2Df kWorldRange = edt::FloatRange2Df::FromMinMax(-init_corner, init_corner);
+inline constexpr Vec2f kInitialCorner{960, 520};
+inline constexpr edt::FloatRange2Df kWorldRange = edt::FloatRange2Df::FromMinMax(-kInitialCorner, kInitialCorner);
 inline constexpr auto kGridSize = 2 + kWorldRange.Extent().Cast<size_t>() / kGridCellSize;
 inline constexpr auto kGridNumCells = kGridSize.x() * kGridSize.y();
 inline constexpr float kTimeStepDurationSeconds = 1.f / 60.f;
 inline constexpr size_t kNumSubSteps = 8;
 inline constexpr float kTimeSubStepDurationSeconds = kTimeStepDurationSeconds / static_cast<float>(kNumSubSteps);
-inline constexpr edt::Vec2f gravity{0.0f, -20.f};
-inline constexpr float kVelocityDampling = 40.f;  // arbitrary, approximating air friction
+inline constexpr edt::Vec2f kGravity{0.0f, -20.f};
+inline constexpr float kVelocityDamping = 40.f;  // arbitrary, approximating air friction
 }  // namespace constants
 }  // namespace verlet
